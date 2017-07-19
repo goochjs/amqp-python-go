71ffb3bff31ce77e3483e844fbaa6fd501a33146
Jeremy Gooch <goochjs@yahoo.co.uk>
2017-07-19 10:18:16 +0100

    Additional config

--- rabbitmq/rabbitmq-commands.sh
+++ rabbitmq/rabbitmq-commands.sh
@@ -6,4 +6,4 @@
 rabbitmqctl set_policy DLX ".*" '{"dead-letter-exchange":"DLX"}' --apply-to queues
 
 # import definitions
-rabbitmqadmin -u user -p user -q import /mnt/rabbitmq/config/rabbitmq-defs.json
+rabbitmqadmin -u admin -p password -q import /mnt/rabbitmq/config/rabbitmq-defs.json
